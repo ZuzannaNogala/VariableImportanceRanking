7b2694d03d1ea3dfcf7d5a5387cac195180da3be
ZuzannaNogala <zuzanna02nogala@gmail.com>
2025-01-25 12:46:22 +0100

    Check how many cases Suc1 and Suc2 are

--- SuccessDefAndPortfolioClust.R
+++ SuccessDefAndPortfolioClust.R
@@ -14,25 +14,31 @@ load("/Users/zuza/Desktop/studia/semestr7/MWM/Data_proj/eventsImpFull.RData")
 sum(is.na(casesFull))
 sum(is.na(eventsImpFull[, .(NumberOfPayment, PaymentAmount)]))
 
+str(casesFull)
+
 ## ZDEFINIOWANIE SKUTECZNOŚCI Z 12 MIESIĘCY
 
 StatsPerCase <- casesFull[, .(CaseId, TOA)][eventsImpFull[, .(CaseId, Month, PaymentAmount, NumberOfPayment)]]
+SummStatsPerCase12M <- StatsPerCase[, .(SumPayments = sum(PaymentAmount), SumNumPayments = sum(NumberOfPayment)), by =.(CaseId, TOA)]
 
-SummStatsPerCase12M <- StatsPerCase[Month <= 12, .(SumPayments = sum(PaymentAmount), SumNumPayments = sum(NumberOfPayment)), by =.(CaseId, TOA)]
 Suc12M <- SummStatsPerCase12M[, .(Suc1 = ifelse(SumPayments / TOA >= 0.05, 1, 0),
                                   Suc2 = ifelse(SumNumPayments >= 2, 1, 0)), 
                               by = CaseId] 
+
+table("suc1" = Suc12M$Suc1, "suc2" = Suc12M$Suc2)
+sum(Suc12M$Suc1)
+sum(Suc12M$Suc2)
 
 Suc12M[, Suc12M := ifelse(Suc2 > Suc1, Suc2, Suc1)]
 Suc12M[, `:=`(Suc1 = NULL, Suc2 = NULL)]
 
 casesFull<- casesFull[Suc12M]
 
-rm(StatsPerCase, Suc12M, SummStatsPerCase12M)
+rm(Suc12M, SummStatsPerCase12M)
 
 ## PODZIAŁ SPRAW NA PORTFOLIA
 
-corrplot(cor(casesFull))
+corrplot(cor(casesFull[, .SD,.SDcols = setdiff(names(casesFull), "CaseId")]), tl.cex = 0.7)
 
 variables <- setdiff(names(casesFull), c("CaseId", "Suc12M"))
 
@@ -73,10 +79,29 @@ names(casesFull)
 
 rm(clusters, banks, v, variables, vnrm)
 
+## SR na Potftel
+
+StatsPerCase <- StatsPerCase[casesFullClust[, .(CaseId, IdPortfolio, Suc12M)]]
+StatsPerCase <- StatsPerCase[, .(IdPortfolio, 
+                                 SumPayments = sum(PaymentAmount), 
+                                 SumNumPayments = sum(NumberOfPayment), 
+                                 Suc12M),
+                             by =.(CaseId, TOA)]
+summary(StatsPerCase)
+
+
+StatsPerCaseSuc <- StatsPerCase[, .(Suc1 = sum(SumPayments)/sum(TOA) >= 0.05,
+                                    Suc2 = mean(SumNumPayments) > 2), by = IdPortfolio]
+
+StatsPerCaseSuc[, SucPerPorfolio := ifelse(Suc2 > Suc1, Suc2, Suc1)]
+StatsPerCaseSuc[, `:=`(Suc1=NULL, Suc2=NULL)]
+StatsPerCaseSuc[, SucPerPorfolio := as.integer(SucPerPorfolio)]
+
 ## ZAPISANIE DANYCH
 
 casesFullClust <- copy(casesFull)
 
+StatsPerCaseSuc
 save(casesFullClust, file = "/Users/zuza/Desktop/studia/semestr7/MWM/Data_proj/casesFullClust.RData")
 save(Portfoliodt,  file = "/Users/zuza/Desktop/studia/semestr7/MWM/Data_proj/PortfolioNamesdt.RData")
 
